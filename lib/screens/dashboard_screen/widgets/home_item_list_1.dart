b6715d7e64926e35710138e4660c4ef3f72944aa
Monda Atef <83195345+MondaAtef@users.noreply.github.com>
2024-05-20 21:04:35 +0200

    recommend_system

--- lib/screens/dashboard_screen/widgets/home_item_list_1.dart
+++ lib/screens/dashboard_screen/widgets/home_item_list_1.dart
@@ -5,6 +5,7 @@ import 'package:vixor_project/const/reusable.dart';
 import 'package:vixor_project/screens/dashboard_screen/widgets/HomeScreen.dart';
 import 'package:vixor_project/screens/dashboard_screen/widgets/Trip/Trip_Screen.dart';
 import 'package:vixor_project/utils/app_imagse.dart';
+import 'package:vixor_project/screens/RecommendationScreen.dart';
 
 class ItemList1 extends StatelessWidget {
   const ItemList1({super.key});
@@ -70,62 +71,66 @@ class ItemList1 extends StatelessWidget {
 
 
 
-            const SizedBox(
+              SizedBox(
               width: 15,
             ),
 
 
-            Expanded(
-                flex: 1,
-                child: GestureDetector(
-                    onTap: () {
-                      navigateTo(context, const TripScreen());
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(40),
-                      ),
-                      child: Stack(
-                        children: [
-                          Positioned.fill(
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(40),
-                              child: Image.asset(
-                                Assets.imagesWall1,
-                                fit: BoxFit
-                                    .cover, // Ensure the image fills the container
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 15,
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(40),
-                              gradient: const LinearGradient(
-                                colors: [
-                                  Color.fromARGB(100, 0, 0, 0),
-                                  Color.fromARGB(100, 0, 0, 0),
-                                ],
-                                stops: [0.0, 0.5],
-                                begin: Alignment.bottomCenter,
-                                end: Alignment.topCenter,
-                              ),
-                            ),
-                            child: Center(
-                              child: CustomTextWidget(
-                                text: "53".tr,
-                                textAlign: TextAlign.center,
-                                fontSize: 19.0,
-                                fontWeight: FontWeight.w900,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ))),
+         //button Show your trip
+             Expanded(
+                 flex: 1,
+                 child: GestureDetector(
+                     onTap: () {
+                       Navigator.push(
+                         context,
+                         MaterialPageRoute(builder: (context) => RecommendationScreen()),
+                       );
+                     },
+                     child: Container(
+                       decoration: BoxDecoration(
+                         borderRadius: BorderRadius.circular(40),
+                       ),
+                       child: Stack(
+                         children: [
+                           Positioned.fill(
+                             child: ClipRRect(
+                               borderRadius: BorderRadius.circular(40),
+                               child: Image.asset(
+                                 Assets.imagesWall1,
+                                 fit: BoxFit
+                                     .cover, // Ensure the image fills the container
+                               ),
+                             ),
+                           ),
+                           const SizedBox(
+                             width: 15,
+                           ),
+                           Container(
+                             decoration: BoxDecoration(
+                               borderRadius: BorderRadius.circular(40),
+                               gradient: const LinearGradient(
+                                 colors: [
+                                   Color.fromARGB(100, 0, 0, 0),
+                                   Color.fromARGB(100, 0, 0, 0),
+                                 ],
+                                 stops: [0.0, 0.5],
+                                 begin: Alignment.bottomCenter,
+                                 end: Alignment.topCenter,
+                               ),
+                             ),
+                             child: Center(
+                               child: CustomTextWidget(
+                                 text: "53".tr,
+                                 textAlign: TextAlign.center,
+                                 fontSize: 19.0,
+                                 fontWeight: FontWeight.w900,
+                                 color: Colors.white,
+                               ),
+                             ),
+                           ),
+                         ],
+                       ),
+                     ))),
           ],
         ),
       ),
