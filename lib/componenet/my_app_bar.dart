2abaf419d97110934631cb7bf8e8c61e6be14721
esraa <em5938941@gmail.com>
2024-06-22 22:54:31 +0300

    appbar

--- lib/componenet/my_app_bar.dart
+++ lib/componenet/my_app_bar.dart
@@ -2,29 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:vixor_project/utils/app_imagse.dart'; // Ensure this import is correct
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-
   // ignore: use_key_in_widget_constructors
   const MyAppBar({Key? key});
 
-
   @override
   Widget build(BuildContext context) {
-    
     return AppBar(
-
       backgroundColor: const Color(0xFF8D502F),
       elevation: 0,
       iconTheme: const IconThemeData(color: Colors.white),
-      title: Center(
-        child: Text(
-          'VIXOR',
-          style: const TextStyle(
+      // title: Center(
+      // child: Text(
+      //   'VIXOR',
+      //   style: const TextStyle(
 
-            fontSize: 30,
-            fontWeight: FontWeight.w400,
-            height: 0.8,
-            color: Color(0xffffffff),
-          ),
+      //     fontSize: 30,
+      //     fontWeight: FontWeight.w400,
+      //     height: 0.8,
+      //     color: Color(0xffffffff),
+      //   ),
+      // ),
+      title: Center(
+        child: Image.asset(
+          Assets.appbar2,
+          width: 140,
+          height: 130,
         ),
       ),
       actions: [
